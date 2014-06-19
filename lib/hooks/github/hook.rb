1e48a5a0256ad7113fc170b8cab03cb8eb628f15
Keita Urashima <ursm@ursm.jp>
2014-06-19 16:55:50 +0900

    Refactoring

--- lib/hooks/github/hook.rb
+++ lib/hooks/github/hook.rb
@@ -1,31 +1,8 @@
+require 'set'
+
 module Idobata::Hook
   class Github < Base
-    name          'GitHub'
-    identifier    :github
-    icon_url      gravatar('61024896f291303615bcd4f7a0dcfb74')
-    form_json_key 'payload'
-    template_name { "#{event_type}.html.haml" }
-
-    helper Helper
-
-    before_render do
-      raise BadRequest, 'This is GitHub hook, who are you?' unless event_type
-
-      skip_processing! if [
-        unsupported_event_types.include?(event_type),
-        event_type == 'pull_request' && payload.action == 'synchronize',
-        event_type == 'push'         && payload.deleted,
-        event_type == 'push'         && payload.commits.blank?,
-      ].any?
-    end
-
-    private
-
-    def event_type
-      headers['X-GitHub-Event']
-    end
-
-    def unsupported_event_types
+    EVENTS_TO_IGNORE = Set.new(
       %w(
         deployment
         fork
@@ -38,6 +15,38 @@ module Idobata::Hook
         public
         team_add
       )
+    )
+
+    name          'GitHub'
+    identifier    :github
+    icon_url      gravatar('61024896f291303615bcd4f7a0dcfb74')
+    form_json_key 'payload'
+    template_name { "#{event_type}.html.haml" }
+
+    helper Helper
+
+    before_render do
+      raise BadRequest, 'This is GitHub hook, who are you?' unless event_type
+
+      skip_processing! if EVENTS_TO_IGNORE.include?(event_type) || synchronize_event? || create_branch_push_event? || delete_branch_push_event?
+    end
+
+    private
+
+    def synchronize_event?
+      event_type == 'pull_request' && payload.action == 'synchronize'
+    end
+
+    def create_branch_push_event?
+      event_type == 'push' && payload.commits.blank?
+    end
+
+    def delete_branch_push_event?
+      event_type == 'push' && payload.deleted
+    end
+
+    def event_type
+      headers['X-GitHub-Event']
     end
   end
 end
