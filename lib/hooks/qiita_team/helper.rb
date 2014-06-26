129f2900c9341162219156f1783963563734c98d
Keita Urashima <ursm@ursm.jp>
2014-06-26 16:17:05 +0900

    Disable mention filter
    The user name of Qiita:Team is not necessarily the same as it of GitHub.

--- lib/hooks/qiita_team/helper.rb
+++ lib/hooks/qiita_team/helper.rb
@@ -2,13 +2,12 @@ module Idobata::Hook
   class QiitaTeam < Base
     module Helper
       filters = [
-        ::HTML::Pipeline::MarkdownFilter,
-        ::HTML::Pipeline::MentionFilter
+        ::HTML::Pipeline::MarkdownFilter
       ]
 
       filters << ::HTML::Pipeline::SyntaxHighlightFilter if defined?(Linguist) # This filter doesn't work on heroku
 
-      Pipeline = ::HTML::Pipeline.new(filters, gfm: true, base_url: 'https://github.com/')
+      Pipeline = ::HTML::Pipeline.new(filters, gfm: true)
 
       def md(source)
         result = Pipeline.call(source)
