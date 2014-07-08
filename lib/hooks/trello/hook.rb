f8a3fa835f5afb1e7d08a3653042e7f9cd06dbd8
Eito Katagiri <eitoball@gmail.com>
2014-07-08 12:56:53 +0900

    add hook for Trello

---
+++ lib/hooks/trello/hook.rb
@@ -0,0 +1,26 @@
+require_relative 'template_name_mapper'
+
+module Idobata::Hook
+  class Trello < Base
+    screen_name   'Trello'
+    identifier    :trello
+    icon_url      'https://s3.amazonaws.com/trello/images/og/trello-icon.png'
+    template_name { custom_template_name }
+
+    helper Helper
+
+    before_render do
+      skip_processing! unless template_name_mapper.call(payload)
+    end
+
+    private
+
+    def custom_template_name
+      template_name_mapper.call(payload)
+    end
+
+    def template_name_mapper
+      @template_name_mapper ||= TemplateNameMapper
+    end
+  end
+end
