f08993158e71f9ea500f54bf20e4251e2afee29f
fukayatsu <fukayatsu@gmail.com>
2014-10-10 22:14:15 +0900

    Add bugsnag support

---
+++ lib/hooks/bugsnag/hook.rb
@@ -0,0 +1,19 @@
+module Idobata::Hook
+  class Bugsnag < Base
+    screen_name 'Bugsnag'
+    identifier  :bugsnag
+    icon_url    gravatar('7e8b5cfa0dbe9df4205ab6d811fddd31')
+    template_name { custom_template_name }
+
+    private
+
+    def custom_template_name
+      case payload.trigger.type
+      when 'comment'
+        'comment.html.haml'
+      else
+        'default.html.haml'
+      end
+    end
+  end
+end
---
+++ spec/bugsnag_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe Idobata::Hook::Bugsnag, type: :hook do
+  let(:payload) { fixture_payload("bugsnag/#{payload_type}.json") }
+
+  before do
+    post payload, 'Content-Type' => 'application/json'
+  end
+
+  describe '#process_payload' do
+    subject { hook.process_payload }
+
+    context 'on comment' do
+      let(:payload_type) { 'comment' }
+      its([:source]) { should eq(<<-HTML.strip_heredoc) }
+        <p>
+          foobarbaz commented on
+          <a href='https://bugsnag.com/example/rails/errors/2345bcds?event_id=12345abc'>[RuntimeError] Bugsnag test exception</a>
+          <blockquote>コメントですよ</blockquote>
+        </p>
+      HTML
+
+      its([:format]) { should eq(:html) }
+    end
+
+    context 'on error' do
+      let(:payload_type) { 'error' }
+      its([:source]) { should eq(<<-HTML.strip_heredoc) }
+        <p>
+          <span class='label label-important'>Exception</span>
+          <a href='https://bugsnag.com/example/rails/errors/2345bcds?event_id=12345abc'>[RuntimeError] Bugsnag test exception</a>
+        </p>
+        <pre>releaseStage: development<br>occurrences:  8</pre>
+      HTML
+
+      its([:format]) { should eq(:html) }
+    end
+  end
+end
