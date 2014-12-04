289294ce4c29081468eb2bfd8e178e95f5433b1d
hibariya <hibariya@gmail.com>
2014-12-04 16:10:20 +0900

    Tweak words

--- spec/airbrake_spec.rb
+++ spec/airbrake_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Idobata::Hook::Airbrake, type: :hook do
-  let(:payload) { fixture_payload("airbrake/error.json") }
+  let(:payload) { fixture_payload('airbrake/error.json') }
 
   before do
     post payload, 'Content-Type' => 'application/json'
@@ -10,8 +10,19 @@ describe Idobata::Hook::Airbrake, type: :hook do
   describe '#process_payload' do
     subject { hook.process_payload }
 
-    it { expect(subject[:source]).to be_present }
-    it { expect(subject[:images]).to be_nil }
-    it { expect(subject[:format]).to eq(:html) }
+    its([:source]) { should eq(<<-HTML.strip_heredoc) }
+      <p>
+        <span class='label label-info'>Airbrake</span>
+        <span class='label label-info'>production</span>
+        <span class='badge badge-important'>118</span>
+      </p>
+      <p>
+        <b>RuntimeError: You threw an exception for testing</b>
+        from
+        <a href='http://airbrake.io:445/pages/exception_test'>http://airbrake.io:445/pages/exception_test</a>
+      </p>
+    HTML
+
+    its([:format]) { should eq(:html) }
   end
 end
