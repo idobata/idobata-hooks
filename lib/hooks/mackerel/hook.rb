c49d92d4e1fed16b949ac1776b167d1f81f9d1d1
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-02-27 12:53:59 +0900

    support Mackerel Webhook

---
+++ lib/hooks/mackerel/hook.rb
@@ -0,0 +1,20 @@
+module Idobata::Hook
+  class Mackerel < Base
+    screen_name 'Mackerel'
+    identifier  :mackerel
+    icon_url    'https://github.com/mackerelio.png'
+    template_name { custom_template_name }
+    helper Helper
+
+    private
+
+    def custom_template_name
+      if payload.event == 'alert'
+        'alert.html.haml'
+      else
+        'default.html.haml'
+      end
+    end
+
+  end
+end
