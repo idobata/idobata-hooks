68f213171a1f8f9d06e85f1eed332aea22feaede
Hika Hibariya <hibariya@gmail.com>
2015-03-20 16:30:42 +0900

    Include styles

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,7 @@
 var pickFiles       = require('broccoli-static-compiler');
 var renderTemplates = require('broccoli-render-template');
 var renameFiles     = require('broccoli-rename-files');
+var mergeTrees      = require('broccoli-merge-trees');
 
 var templates = pickFiles('lib/hooks', {
   srcDir:  '/',
@@ -9,9 +10,16 @@ var templates = pickFiles('lib/hooks', {
 });
 
 templates = renderTemplates(templates);
-
-module.exports = renameFiles(templates, {
+templates = renameFiles(templates, {
   transformFilename: function(filename, basename, extname) {
     return basename.replace('.html', '.hbs');
   }
 });
+
+var styles = pickFiles('lib/hooks', {
+  srcDir: '/',
+  files:  ['*/style.sass'],
+  destDir: 'styles/idobata-hooks'
+});
+
+module.exports = mergeTrees([templates, styles]);
