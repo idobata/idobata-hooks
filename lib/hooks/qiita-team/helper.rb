88f5c367a364098664257a8ad20e175cc7da866b
Keita Urashima <ursm@ursm.jp>
2015-08-17 12:02:01 +0900

    Fix broken emoji

--- lib/hooks/qiita-team/helper.rb
+++ lib/hooks/qiita-team/helper.rb
@@ -1,23 +1,6 @@
 module Idobata::Hook
   class QiitaTeam < Base
     module Helper
-      class QiitaHTMLFilter < HTML::Pipeline::Filter
-        def call
-          doc.tap {
-            doc.search('span.fragment, span.fragment + a').each(&:remove)
-
-            doc.search('img.emoji').each do |emoji|
-              emoji.replace emoji_image_filter(emoji[:title])
-            end
-          }
-        end
-
-        # I'll forget I saw it :bow:
-        def emoji_image_filter(text)
-          HTML::Pipeline::EmojiFilter.new(nil, asset_root: '/images').emoji_image_filter(text)
-        end
-      end
-
       filters = [
         ::HTML::Pipeline::MarkdownFilter
       ]
@@ -30,10 +13,6 @@ module Idobata::Hook
         result = Pipeline.call(source)
 
         result[:output].to_s.html_safe
-      end
-
-      def fixup_qiita_html(html)
-        QiitaHTMLFilter.new(html).call.to_s.html_safe
       end
     end
   end
