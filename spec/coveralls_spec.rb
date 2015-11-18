4722301b6d04cbfc87948d29624147dfa093be90
Hika Hibariya <hibariya@gmail.com>
2015-11-18 16:21:58 +0900

    Fix specs :expressionless:

--- spec/coveralls_spec.rb
+++ spec/coveralls_spec.rb
@@ -13,14 +13,12 @@ describe Idobata::Hook::Coveralls, type: :hook do
 
       its([:source]) { should eq(<<-HTML.strip_heredoc) }
       <p>
-        <a href='https://example.io/builds/987654321'>
-          gihub-user/repo-name
-        </a>
+        <a href='https://example.io/builds/987654321'>gihub-user/repo-name</a>
         coverage increased
         <span class='badge progress-bar-success'>3.50</span>
         to
         <span class='label label-info'>99.35%</span>
-        on branch-name after commit
+        on branch-name
       </p>
       <p>
         commit message by Committer Name &lt;user@example.com&gt;
@@ -35,14 +33,12 @@ describe Idobata::Hook::Coveralls, type: :hook do
 
       its([:source]) { should eq(<<-HTML.strip_heredoc) }
       <p>
-        <a href='https://example.io/builds/987654321'>
-          gihub-user/repo-name
-        </a>
+        <a href='https://example.io/builds/987654321'>gihub-user/repo-name</a>
         coverage decreased
         <span class='badge progress-bar-danger'>-0.80</span>
         to
         <span class='label label-info'>88.19%</span>
-        on branch-name after commit
+        on branch-name
       </p>
       <p>
         commit message by Committer Name &lt;user@example.com&gt;
@@ -57,12 +53,10 @@ describe Idobata::Hook::Coveralls, type: :hook do
 
       its([:source]) { should eq(<<-HTML.strip_heredoc) }
       <p>
-        <a href='https://example.io/builds/987654321'>
-          gihub-user/repo-name
-        </a>
+        <a href='https://example.io/builds/987654321'>gihub-user/repo-name</a>
         coverage remained the same at
         <span class='label label-info'>100.00%</span>
-        on branch-name after commit
+        on branch-name
       </p>
       <p>
         commit message by Committer Name &lt;user@example.com&gt;
@@ -77,12 +71,10 @@ describe Idobata::Hook::Coveralls, type: :hook do
 
       its([:source]) { should eq(<<-HTML.strip_heredoc) }
       <p>
-        <a href='https://example.io/builds/987654321'>
-          gihub-user/repo-name
-        </a>
+        <a href='https://example.io/builds/987654321'>gihub-user/repo-name</a>
         coverage remained the same at
         <span class='label label-info'>0.00%</span>
-        on branch-name after commit
+        on branch-name
       </p>
       <p>
         commit message by Committer Name &lt;user@example.com&gt;
