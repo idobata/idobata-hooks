390f5e5e9a98e6c7c2caae1da0cd0769174779be
TAKAHASHI Kazunari <takahashi@1syo.net>
2015-12-14 14:39:50 +0900

    Configure: VCR

---
+++ spec/support/vcr.rb
@@ -0,0 +1,6 @@
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/vcr'
+  config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
+  config.ignore_localhost = true
+end
