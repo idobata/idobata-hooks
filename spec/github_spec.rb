848fc242f18bb830a426bf74b391cb35221553a0
Hibariya <hibariya@gmail.com>
2016-10-19 14:24:05 +0900

    :lipstick:

--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -404,8 +404,7 @@ describe Idobata::Hook::Github, type: :hook do
           <p>
             <span><img src="https://avatars.githubusercontent.com/u/17717895?v=3" width="16" height="16" alt="" /></span>
             <a href='https://github.com/obatan'>obatan</a>
-            commented
-            on pull request
+            reviewed pull request
             <a href='https://github.com/idobata/idobata-hooks/pull/73#pullrequestreview-4794622'>idobata/idobata-hooks#73</a>
           </p>
           <p>leaving a review comment</p>
@@ -420,8 +419,7 @@ describe Idobata::Hook::Github, type: :hook do
           <p>
             <span><img src="https://avatars.githubusercontent.com/u/17717895?v=3" width="16" height="16" alt="" /></span>
             <a href='https://github.com/obatan'>obatan</a>
-            approved
-            on pull request
+            approved pull request
             <a href='https://github.com/idobata/idobata-hooks/pull/73#pullrequestreview-4794931'>idobata/idobata-hooks#73</a>
           </p>
           <p>I'd like to approve this :robot: </p>
@@ -436,8 +434,7 @@ describe Idobata::Hook::Github, type: :hook do
           <p>
             <span><img src="https://avatars.githubusercontent.com/u/17717895?v=3" width="16" height="16" alt="" /></span>
             <a href='https://github.com/obatan'>obatan</a>
-            requested changes
-            on pull request
+            requested changes on pull request
             <a href='https://github.com/idobata/idobata-hooks/pull/73#pullrequestreview-4794838'>idobata/idobata-hooks#73</a>
           </p>
           <p>We'd like to change ...</p>
