46361e0df61721b266fcd069268fb5b89fb26893
Seiei Miyagi <hanachin@gmail.com>
2017-06-06 11:45:46 +0900

    Rename screen_name: Docker Hub automated build -> Docker Hub

--- lib/hooks/docker-hub/hook.rb
+++ lib/hooks/docker-hub/hook.rb
@@ -1,6 +1,6 @@
 module Idobata::Hook
   class DockerHub < Base
-    screen_name 'Docker Hub automated build'
+    screen_name 'Docker Hub'
     icon_url hook_image_url('mono_vertical_small.png')
   end
 end
