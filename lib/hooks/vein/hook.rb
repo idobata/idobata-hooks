127f7f2e11d3930d701abf7e05674334376761b8
niku <niku@niku.name>
2019-02-14 12:15:26 +0900

    Also username and body

--- lib/hooks/vein/hook.rb
+++ lib/hooks/vein/hook.rb
@@ -5,21 +5,14 @@ module Idobata::Hook
 
     def usericon
       payload.icon_url
-      # This value is used for a url of an image.
-      # If it returns a warning message, it shows just broken image to users.
-      # So, you should not make a warning message here.
     end
 
     def username
       payload.username.gsub(/ \(VeinBot\)$/, '')
-    rescue
-      '__WARNING: It could not make "username", please check your idobata hook settings.__'
     end
 
     def body
       payload.text.gsub(/\nshared by Vein$/, '')
-    rescue
-      '__WARNING: It could not make "body", please check your idobata hook settings.__'
     end
   end
 end
