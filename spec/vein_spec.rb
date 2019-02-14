7bad608fcb0f18057fcffd5d78d46bd185adf354
niku <niku@niku.name>
2019-02-14 12:53:13 +0900

    Revise hook message

--- spec/vein_spec.rb
+++ spec/vein_spec.rb
@@ -10,7 +10,7 @@ describe Idobata::Hook::Vein, type: :hook do
 
     it { expect(subject[:source]).to be_dom_equal <<~HTML }
       <img height='16' src='https://www.gravatar.com/avatar/a51816c3b11f79d9644a46105d513f1b?default=identicon&s=48' width='16'>
-      <b>niku</b>
+      <b>niku starred</b>
       <br>
       Elixir v1.8 released
       <br>
